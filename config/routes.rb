265b1f026a6c45401c30a642358cca77c71f9349
PavanRaaj <pavanraajgoud@gmail.com>
2022-10-10 19:28:38 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  get 'pages/index'
+
+  root to: 'pages#index'
+
+  resources :users, only: [:new, :create]
+
+resources :user_sessions, only: [:create, :destroy]
+
+delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+get '/sign_in', to: 'user_sessions#new', as: :sign_in
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
